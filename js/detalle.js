e231fd3a908294fa43f0f3e535297e738d0fee66
martincavigliasso <martincavigliasso@gmail.com>
2022-08-25 14:40:30 -0300

    modifique

--- js/detalle.js
+++ js/detalle.js
@@ -36,10 +36,10 @@ function mostrarDetalle(obj) {
                                         </div>
                                         <div class="row sub-para-w3pvt my-5">
 
-                                            <h3 class="shop-sing">Lorem ipsum dolor sit amet</h3>
-                                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elPellentesque vehicula augue eget nisl ullamcorper, molestie blandit ipsum auctor. Mauris volutpat augue dolor.Consectetur adipisicing elit, sed do eiusmod tempor incididunt ut lab ore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco. labore et dolore magna aliqua.</p>
-                                            <p class="mt-3 italic-blue">Consectetur adipisicing elPellentesque vehicula augue eget nisl ullamcorper, molestie blandit ipsum auctor. Mauris volutpat augue dolor.Consectetur adipisicing elit, sed do eiusmod tempor incididunt ut lab ore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco. labore et dolore magna aliqua.</p>
-                                            <p class="mt-3">Lorem ipsum dolor sit amet, consectetur adipisicing elPellentesque vehicula augue eget nisl ullamcorper, molestie blandit ipsum auctor. Mauris volutpat augue dolor.Consectetur adipisicing elit, sed do eiusmod tempor incididunt ut lab ore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco. labore et dolore magna aliqua.</p>
+                                            <h3 class="shop-sing"></h3>
+                                            <p></p>
+                                            <p class="mt-3 italic-blue"></p>
+                                            <p class="mt-3"></p>
                                         </div>
                                     </div>
                                 </div>`
